5de8814e83dc89e9ea584c59878140d3ea7b9976
Gabriel Salerno <160797866+GabrielSalerno@users.noreply.github.com>
2024-03-04 09:43:53 -0300

    Add files via upload

--- ac1/ac1.py
+++ ac1/ac1.py
@@ -1,13 +1,12 @@
 # Exercício 1
-def calculadora_equacao_segundo_grau(a, b, c):
+def calculadora_equacao_segundo_grau():
+    a = int(input("Informe o parâmetro a da equação: "))
+    b = int(input("Informe o parâmetro b da equação: "))
+    c = int(input("Informe o parâmetro c da equação: "))
     print("A primeira raiz da equação é", (-b + ((b**2-4*a*c)**(1/2)))/(2*a))
     print("A segunda raiz da equação é", (-b - ((b**2-4*a*c)**(1/2)))/(2*a))
 
-a_user = int(input("Informe o parâmetro a da equação: "))
-b_user = int(input("Informe o parâmetro b da equação: "))
-c_user = int(input("Informe o parâmetro c da equação: "))
-
-calculadora_equacao_segundo_grau(a_user, b_user, c_user)
+calculadora_equacao_segundo_grau()
 
 # Exercício 2
 ano = int(input("Informe o ano: "))
